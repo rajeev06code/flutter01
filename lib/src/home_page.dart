00d197aab55f263fe96b7149e0b321d34b605869
rajeev06code <rajeev_sharma15@outlook.com>
2023-04-23 12:08:35 +0530

    day_1

---
+++ lib/src/home_page.dart
@@ -0,0 +1,19 @@
+import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Catalogue App')),
+      body: Center(
+        child: Container(
+          child: const Text("welcome to 30 days challenge"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
